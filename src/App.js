2a656e5e36336e4d64aed0c7eeb6a06a4509946d
gaagul <gaagulz@gmail.com>
2022-03-28 16:20:49 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './Pages/About';
+import Properties from './Pages/Properties';
+import SingleProperty from './Pages/SingleProperty';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yooo!</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+            <Route path="/" exact component />
+            <Route path="Properties" exact component={Properties} />
+            <Route path="About" exact component={About} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
