49fa7c5101b0383afb6111fdba2a70c13a1c4b5b
gaagul <gaagulz@gmail.com>
2022-05-05 22:46:05 +0530

    rent page added yet to design

---
+++ src/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from '../components/hero/Hero'
+import Best from '../components/best/Best'
+import Featured from '../components/featured/Featured'
+import Footer from '../components/footer/Footer'
+function Home() {
+  return (
+    <>
+    <Hero />
+    <Best />
+    <Featured />
+    <Footer />
+    </>
+  );
+}
+
+export default Home;
---
+++ src/Pages/Rent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Contacts from './Properties/Contacts'
+
+const Rent = () => {
+  return (
+    <>
+      <Contacts />
+      <Contacts />
+      <Contacts />
+      <Contacts />
+      <Contacts />
+      <Contacts />
+      <Contacts />
+    </>
+  );
+}
+export default Rent;
