0f1138a2f21b74264e1624fc7d2d8b6597f92ea5
gaagul <gaagulz@gmail.com>
2022-05-28 00:20:12 +0530

    added visit and owner info

--- src/Pages/SingleProperty.js
+++ src/Pages/SingleProperty.js
@@ -46,54 +46,54 @@ const SingleProperty = () => {
                                             </div>
                                         </div>
                                     </section> 
-                                    <div id="listingDetailsSlider" class="carousel listing-details-sliders slide mb-30">
-                                    <h5 class="mb-4">Gallery</h5>
-                                    <div class="carousel-inner">
-                                        <div class="item carousel-item active" data-slide-number="0">
-                                            <img src={house} class="img-fluid" alt="slider-listing"></img>
+                                    <div id="listingDetailsSlider" className="carousel listing-details-sliders slide mb-30">
+                                    <h5 className="mb-4">Gallery</h5>
+                                    <div className="carousel-inner">
+                                        <div className="item carousel-item active" data-slide-number="0">
+                                            <img src={house} className="img-fluid" alt="slider-listing"></img>
                                         </div>
-                                        <div class="item carousel-item" data-slide-number="1">
-                                            <img src={house1} class="img-fluid" alt="slider-listing"></img>
+                                        <div className="item carousel-item" data-slide-number="1">
+                                            <img src={house1} className="img-fluid" alt="slider-listing"></img>
                                         </div>
-                                        <div class="item carousel-item active" data-slide-number="2">
-                                            <img src={house2} class="img-fluid" alt="slider-listing"></img>
+                                        <div className="item carousel-item active" data-slide-number="2">
+                                            <img src={house2} className="img-fluid" alt="slider-listing"></img>
                                         </div>
-                                        <div class="item carousel-item" data-slide-number="4">
-                                            <img src={house3} class="img-fluid" alt="slider-listing"></img>
+                                        <div className="item carousel-item" data-slide-number="4">
+                                            <img src={house3} className="img-fluid" alt="slider-listing"></img>
                                         </div>
-                                        <div class="item carousel-item" data-slide-number="5">
-                                            <img src={house4} class="img-fluid" alt="slider-listing"></img>
+                                        <div className="item carousel-item" data-slide-number="5">
+                                            <img src={house4} className="img-fluid" alt="slider-listing"></img>
                                         </div>
 
-                                        <a class="carousel-control left" href="#listingDetailsSlider" data-slide="prev"><i class="fa fa-angle-left"></i></a>
-                                        <a class="carousel-control right" href="#listingDetailsSlider" data-slide="next"><i class="fa fa-angle-right"></i></a>
+                                        <a className="carousel-control left" href="#listingDetailsSlider" data-slide="prev"><i className="fa fa-angle-left"></i></a>
+                                        <a className="carousel-control right" href="#listingDetailsSlider" data-slide="next"><i className="fa fa-angle-right"></i></a>
 
                                     </div>
                                     {/* <!-- main slider carousel nav controls --> */}
-                                    <ul class="carousel-indicators smail-listing list-inline">
-                                        <li class="list-inline-item">
-                                            <a id="carousel-selector-0" class="selected" data-slide-to="0" data-target="#listingDetailsSlider">
-                                                <img src={house} class="img-fluid" alt="listing-small"></img>
+                                    <ul className="carousel-indicators smail-listing list-inline">
+                                        <li className="list-inline-item">
+                                            <a id="carousel-selector-0" className="selected" data-slide-to="0" data-target="#listingDetailsSlider">
+                                                <img src={house} className="img-fluid" alt="listing-small"></img>
                                             </a>
                                         </li>
-                                        <li class="list-inline-item">
+                                        <li className="list-inline-item">
                                             <a id="carousel-selector-1" data-slide-to="1" data-target="#listingDetailsSlider">
-                                                <img src={house1} class="img-fluid" alt="listing-small"></img>
+                                                <img src={house1} className="img-fluid" alt="listing-small"></img>
                                             </a>
                                         </li>
-                                        <li class="list-inline-item active">
+                                        <li className="list-inline-item active">
                                             <a id="carousel-selector-2" data-slide-to="2" data-target="#listingDetailsSlider">
-                                                <img src={house2} class="img-fluid" alt="listing-small"></img>
+                                                <img src={house2} className="img-fluid" alt="listing-small"></img>
                                             </a>
                                         </li>
-                                        <li class="list-inline-item">
+                                        <li className="list-inline-item">
                                             <a id="carousel-selector-3" data-slide-to="3" data-target="#listingDetailsSlider">
-                                                <img src={house3} class="img-fluid" alt="listing-small"></img>
+                                                <img src={house3} className="img-fluid" alt="listing-small"></img>
                                             </a>
                                         </li>
-                                        <li class="list-inline-item">
+                                        <li className="list-inline-item">
                                             <a id="carousel-selector-4" data-slide-to="4" data-target="#listingDetailsSlider">
-                                                <img src={house4} class="img-fluid" alt="listing-small"></img>
+                                                <img src={house4} className="img-fluid" alt="listing-small"></img>
                                             </a>
                                         </li>
                                     </ul>
@@ -102,54 +102,109 @@ const SingleProperty = () => {
                                 </div>
                             </div>
 
-                            <div class="single homes-content details mb-30">
+                            <div className="single homes-content details mb-30">
                             
-                                <h5 class="mb-4">Property Details</h5>
-                                <ul class="homes-list clearfix">
+                                <h5 className="mb-4">Property Details</h5>
+                                <ul className="homes-list clearfix">
                                     <li>
-                                        <span class="font-weight-bold mr-1">Property ID:</span>
-                                        <span class="det">V254680</span>
+                                        <span className="font-weight-bold mr-1">Property ID:</span>
+                                        <span className="det">V254680</span>
                                     </li>
                                     <li>
-                                        <span class="font-weight-bold mr-1">Property Type:</span>
-                                        <span class="det">House</span>
+                                        <span className="font-weight-bold mr-1">Property Type:</span>
+                                        <span className="det">House</span>
                                     </li>
                                     <li>
-                                        <span class="font-weight-bold mr-1">Property status:</span>
-                                        <span class="det">For Sale</span>
+                                        <span className="font-weight-bold mr-1">Property status:</span>
+                                        <span className="det">For Sale</span>
                                     </li>
                                     <li>
-                                        <span class="font-weight-bold mr-1">Property Price:</span>
-                                        <span class="det">$230,000</span>
+                                        <span className="font-weight-bold mr-1">Property Price:</span>
+                                        <span className="det">$230,000</span>
                                     </li>
                                     <li>
-                                        <span class="font-weight-bold mr-1">Rooms:</span>
-                                        <span class="det">6</span>
+                                        <span className="font-weight-bold mr-1">Rooms:</span>
+                                        <span className="det">6</span>
                                     </li>
                                     <li>
-                                        <span class="font-weight-bold mr-1">Bedrooms:</span>
-                                        <span class="det">7</span>
+                                        <span className="font-weight-bold mr-1">Bedrooms:</span>
+                                        <span className="det">7</span>
                                     </li>
                                     <li>
-                                        <span class="font-weight-bold mr-1">Bath:</span>
-                                        <span class="det">4</span>
+                                        <span className="font-weight-bold mr-1">Bath:</span>
+                                        <span className="det">4</span>
                                     </li>
                                     <li>
-                                        <span class="font-weight-bold mr-1">Garages:</span>
-                                        <span class="det">2</span>
+                                        <span className="font-weight-bold mr-1">Garages:</span>
+                                        <span className="det">2</span>
                                     </li>
                                     <li>
-                                        <span class="font-weight-bold mr-1">Year Built:</span>
-                                        <span class="det">10/6/2020</span>
+                                        <span className="font-weight-bold mr-1">Year Built:</span>
+                                        <span className="det">10/6/2020</span>
                                     </li>
                                 </ul>
                             </div>
                             
-                            <div class="floor-plan property wprt-image-video w50 pro">
+                            <div className="floor-plan property wprt-image-video w50 pro">
                                 <h5>Floor Plans</h5>
                                 <img alt="image" src={fp}></img>
                             </div>
                         </div>
+                        
+                        
+                        
+                        <aside class="col-lg-4 col-md-12 car">
+                            <div class="single widget">
+                                <div className="schedule widget-boxed mt-33 mt-0">
+                                        <div className="widget-boxed-header">
+                                            <h4><i className="fa fa-calendar pr-3 padd-r-10"></i>Schedule a Tour</h4>
+                                        </div>
+                                        <div className="widget-boxed-body">
+                                            <div className="row">
+                                                <div className="col-lg-6 col-md-12 book">
+                                                    <input type="text" id="reservation-date" data-lang="en" data-large-mode="true" data-min-year="2017" data-max-year="2020" data-disabled-days="08/17/2017,08/18/2017" data-id="datedropper-0" data-theme="my-style" className="form-control picker-input" readonly=""></input>
+                                                </div>
+                                                <div className="col-lg-6 col-md-12 book2">
+                                                    <input type="text" id="reservation-time" className="form-control td-input" readonly=""></input>
+                                                </div>
+                                            </div>
+                                            <a href="payment-method.html" className="btn reservation btn-radius theme-btn full-width mrg-top-10">Submit Request</a>
+                                        </div>
+                                </div>
+                            </div>
+                            
+                            
+                            <div class="widget-boxed mt-33 mt-5">
+                                    <div class="widget-boxed-header">
+                                        <h4>Seller Information</h4>
+                                    </div>
+                                    <div class="widget-boxed-body">
+                                        <div class="sidebar-widget author-widget2">
+                                            <div class="author-box clearfix">
+                                                <img src="images/testimonials/ts-1.jpg" alt="author-image" class="author__img"></img>
+                                                <h4 class="author__title">Lisa Clark</h4>
+                                                <p class="author__meta">Agent of Property</p>
+                                            </div>
+                                            <ul class="author__contact">
+                                                <li><span class="la la-map-marker"><i class="fa fa-map-marker"></i></span>302 Av Park, New York</li>
+                                                <li><span class="la la-phone"><i class="fa fa-phone" aria-hidden="true"></i></span><a href="#">(234) 0200 17813</a></li>
+                                                <li><span class="la la-envelope-o"><i class="fa fa-envelope" aria-hidden="true"></i></span><a href="#">lisa@gmail.com</a></li>
+                                            </ul>
+                                            <div class="agent-contact-form-sidebar">
+                                                <h4>Request Inquiry</h4>
+                                                <form name="contact_form" method="post" action="functions.php">
+                                                    <input type="text" id="fname" name="full_name" placeholder="Full Name" required=""></input>
+                                                    <input type="number" id="pnumber" name="phone_number" placeholder="Phone Number" required=""></input>
+                                                    <input type="email" id="emailid" name="email_address" placeholder="Email Address" required=""></input>
+                                                    <textarea placeholder="Message" name="message" required=""></textarea>
+                                                    <input type="submit" name="sendmessage" class="multiple-send-message" value="Submit Request"></input>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                        </aside>
+
                     </div>
 
                 </div>
