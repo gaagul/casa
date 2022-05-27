c37d32f28067226757099c3ed68c03325883d7b8
gaagul <gaagulz@gmail.com>
2022-05-27 21:18:39 +0530

    single property page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Pages/Home';
 import About from './Pages/About';
 import Rent from './Pages/Rent';
 import Signup from './Pages/Signup'
+import SingleProperty from './Pages/SingleProperty'
 
 
 // import Sell from './Pages/Sell';
@@ -18,7 +19,7 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-      <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/" element={<SingleProperty/>}/>
       <Route path="/Rent"  element={<Rent />} />
       <Route path="/Signup"  element={<Signup />} />
       <Route path="/Signin"  element={<Signin />} />
